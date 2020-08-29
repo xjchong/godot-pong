786ae6038f6499b25aaeacb23165ec45daa2eac6
Xavier <xavier.chong.j@gmail.com>
2020-08-28 20:24:10 -0400

    Refactor win screen to use audio manager

--- scenes/win_screen/WinScreen.gd
+++ scenes/win_screen/WinScreen.gd
@@ -4,22 +4,19 @@ extends CanvasLayer
 
 onready var blur_out_animation: AnimationPlayer = $ColorRect/BlurOutAnimation
 onready var winner_dialog: WindowDialog = $ColorRect/CenterContainer/WinnerDialog
-onready var win_audio: AudioStreamPlayer = $WinAudio
-onready var hover_audio: AudioStreamPlayer = $HoverAudio
-onready var confirm_audio: AudioStreamPlayer = $ConfirmAudio
 
 
 func show(player_id: int):
 	winner_dialog.dialog_text = "Player %s wins!" % player_id
 	winner_dialog.get_close_button().hide()
 	blur_out_animation.play("BlurOutAnimation")
-	win_audio.play()
+	AudioManager.play(Audio.VICTORY)
 	yield(blur_out_animation, "animation_finished")
 	winner_dialog.show()
 
 
 func _on_AcceptDialog_confirmed():
-	confirm_audio.play()
+	AudioManager.play(Audio.PRESS)
 	yield(confirm_audio, "finished")
 	get_tree().paused = false
 	get_tree().change_scene(GlobalPath.MAIN_MENU)
