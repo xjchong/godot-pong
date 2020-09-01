d4f72dc62ccece8b59f6cfaa55fee579d123492b
Xavier <xavier.chong.j@gmail.com>
2020-09-01 13:17:30 -0400

    Add background color constants

--- autoload/game_color/GameColor.gd
+++ autoload/game_color/GameColor.gd
@@ -1,5 +1,14 @@
 extends Node
 
 
+const BACKGROUND_BLUE = Color("0092ed")
+const BACKGROUND_GRAY = Color("2e2e2e")
+const BACKGROUND_GREEN = Color("7ac48a")
+const BACKGROUND_MAGENTA = Color("db006a")
+const BACKGROUND_PINK = Color("c47e7a")
+const BACKGROUND_PURPLE = Color("190331")
+const BACKGROUND_TAN = Color("c4b87a")
+const BACKGROUND_YELLOW = Color("fcba03")
 const FOREGROUND: Color = Color("fdf5e3")
 const FOCUS: Color = Color("b7fdf5e3")
+
